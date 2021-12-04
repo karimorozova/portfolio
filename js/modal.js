1506c3ebeeff5095a791c67a0d8a61d1dbd638a7
karine-morozova <morozova_karine@ukr.net>
2021-12-04 23:02:01 +0200

    Add some styles for header

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('.header-btn'),
+      closeModalBtn: document.querySelector('.close-btn'),
+      modal: document.querySelector('.backdrop'),
+      modalItem: document.querySelector('.modal')
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.modal.addEventListener('click', removeModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+    function removeModal(e) {
+      if (e.target === refs.modal) {
+        refs.modal.classList.add('is-hidden');
+      }
+    }
+  })();
